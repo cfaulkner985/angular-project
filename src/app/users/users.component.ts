825c5e601884e9300a863f6ce7d42d01732bb43f
cfaulkner985 <cfaulkner985@gmail.com>
2023-02-03 14:44:35 +0000

    Added styles and classes

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class UsersComponent implements OnInit {
   userCreationStatus = 'No User was created!';
   userName = '';
+  userCreated = false;
+  users = ['User', 'User 2']
+  log = []
 
   ngOnInit(): void {
   }
 
   onCreateUser() {
+    this.users.push(this.userName);
     this.userCreationStatus = 'User was created! Name is ' + this.userName;
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
   onUpdateUserName(event: Event) {
